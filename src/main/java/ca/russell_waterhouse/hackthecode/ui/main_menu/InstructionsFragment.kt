b169bc13b7ac60e51c7b14ffeadbe2d5f20c0c14
Russell Waterhouse <russell.l.waterhouse@gmail.com>
2020-01-08 12:01:37 -0800

    updated the InstructionsFragment to not take parameters from the newInstance Method

--- src/main/java/ca/russell_waterhouse/hackthecode/ui/main_menu/InstructionsFragment.kt
+++ src/main/java/ca/russell_waterhouse/hackthecode/ui/main_menu/InstructionsFragment.kt
@@ -10,32 +10,17 @@ import android.view.ViewGroup
 
 import ca.russell_waterhouse.hackthecode.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  * Activities that contain this fragment must implement the
- * [instructionsFragment.OnFragmentInteractionListener] interface
+ * [InstructionsFragment.OnFragmentInteractionListener] interface
  * to handle interaction events.
- * Use the [instructionsFragment.newInstance] factory method to
+ * Use the [InstructionsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class instructionsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class InstructionsFragment : Fragment() {
     private var listener: OnFragmentInteractionListener? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,11 +28,6 @@ class instructionsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_instructions, container, false)
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
     }
 
     override fun onAttach(context: Context) {
@@ -76,8 +56,6 @@ class instructionsFragment : Fragment() {
      * for more information.
      */
     interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
     }
 
     companion object {
@@ -85,17 +63,12 @@ class instructionsFragment : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment instructionsFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            instructionsFragment().apply {
+        fun newInstance() =
+            InstructionsFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
