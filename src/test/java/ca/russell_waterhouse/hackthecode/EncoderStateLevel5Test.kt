5dfa2485305771612a69677a84a92959ff7f27ee
Russell Waterhouse <russell.l.waterhouse@gmail.com>
2020-01-22 14:34:27 -0800

    Implemented EncoderStateLevel5Test.kt

--- src/test/java/ca/russell_waterhouse/hackthecode/EncoderStateLevel5Test.kt
+++ src/test/java/ca/russell_waterhouse/hackthecode/EncoderStateLevel5Test.kt
@@ -1,19 +1,28 @@
 package ca.russell_waterhouse.hackthecode
 
-import org.junit.Before
+import ca.russell_waterhouse.hackthecode.model.encoding_states.EncoderStateLevel5
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class EncoderStateLevel5Test {
 
-    @Before
-    fun setUp() {
-    }
-
     @Test
     fun getLevel() {
+        val encoderState = EncoderStateLevel5()
+        assertEquals(5, encoderState.getLevel())
     }
 
     @Test
     fun encode() {
+        val encoderState = EncoderStateLevel5()
+        assertEquals("I", encoderState.encode("E"))
+        assertEquals("u", encoderState.encode("o"))
+        assertEquals("3", encoderState.encode("3"))
+        assertEquals("d", encoderState.encode("c"))
+        assertEquals("D", encoderState.encode("C"))
+        assertEquals("B", encoderState.encode("Z"))
+        assertEquals("b", encoderState.encode("z"))
+        assertEquals("fsux", encoderState.encode("word"))
+        assertEquals(" tfsux imqovman", encoderState.encode("multiple words "))
     }
 }
