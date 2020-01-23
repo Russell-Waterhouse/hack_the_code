b6e750a2c2e766021208980a009f5f41d9db350d
Russell <Russell.L.Waterhouse@gmail.com>
2020-01-22 22:40:35 -0800

    moved git repository to parent directory

---
+++ app/src/main/java/ca/russell_waterhouse/hackthecode/model/encoding_states/EncoderStateLevel8.kt
@@ -0,0 +1,20 @@
+package ca.russell_waterhouse.hackthecode.model.encoding_states
+
+class EncoderStateLevel8: EncoderState() {
+    override fun getLevel(): Int {
+        return 8
+    }
+
+    override fun encode(word: String): String {
+        var previousLetter = word[0]
+        var currentLetter= word[1]
+        for ()
+        if () {
+            if (! previousLetter.)
+        }
+    }
+
+    private fun numberFromLetter(letter: Char): Int{
+        return (letter.toInt() - 'A'.toInt() + 1)
+    }
+}
