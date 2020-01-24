765e3dd2c008cf845d9b5244604c2f463c4b6323
Russell Waterhouse <russell.l.waterhouse@gmail.com>
2020-01-24 10:10:57 -0800

    implemented EncoderStateLevel8.kt and created and implemented EncoderStateLevel8Test.kt

--- app/src/main/java/ca/russell_waterhouse/hackthecode/model/encoding_states/EncoderStateLevel8.kt
+++ app/src/main/java/ca/russell_waterhouse/hackthecode/model/encoding_states/EncoderStateLevel8.kt
@@ -6,12 +6,38 @@ class EncoderStateLevel8: EncoderState() {
     }
 
     override fun encode(word: String): String {
-        var previousLetter = word[0]
-        var currentLetter= word[1]
-        return "null"
+        val numbers = IntArray(word.length)
+        var i = 0
+        for (letter: Char in word){
+            numbers[i] = numberFromLetter(letter)
+            i++
+        }
+        i = 1
+        var newNumbers = numbers.copyOf()
+        while (i < numbers.size - 1){
+            newNumbers[i] = numbers[i] - numbers[i-1]
+            i++
+        }
+        newNumbers = newNumbers.reversedArray()
+        i = 1
+        numbers[0] = newNumbers[0]
+        while (i < numbers.size){
+            numbers[i] = newNumbers[i] + newNumbers[i-1]
+            i++
+        }
+        return intArrayToString(numbers)
     }
 
     private fun numberFromLetter(letter: Char): Int{
         return (letter.toInt() - 'A'.toInt() + 1)
+    }
+
+    private fun intArrayToString(array: IntArray): String{
+        val builder = StringBuilder()
+        for (i in array){
+            builder.append(i)
+            builder.append(",")
+        }
+        return builder.toString()
     }
 }
---
+++ app/src/test/java/ca/russell_waterhouse/hackthecode/model/encoding_states/EncoderStateLevel8Test.kt
@@ -0,0 +1,20 @@
+package ca.russell_waterhouse.hackthecode.model.encoding_states
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class EncoderStateLevel8Test {
+
+    @Test
+    fun getLevel() {
+        val encoderState = EncoderStateLevel8()
+        assertEquals(8, encoderState.getLevel())
+    }
+
+    @Test
+    fun encode() {
+        val encoderState = EncoderStateLevel8()
+        assertEquals("1,", encoderState.encode("A"))
+        TODO("Testing not yet completed")
+    }
+}
