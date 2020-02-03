86303932d0d592cdf698beb29f8cbba71a066935
Russell Waterhouse <russell.l.waterhouse@gmail.com>
2020-02-03 12:05:56 -0800

    added test for level 8 decimals

--- app/src/test/java/ca/russell_waterhouse/hackthecode/model/encoding_states/EncoderStateLevel8Test.kt
+++ app/src/test/java/ca/russell_waterhouse/hackthecode/model/encoding_states/EncoderStateLevel8Test.kt
@@ -20,7 +20,7 @@ class EncoderStateLevel8Test {
                 ",-69,-62,14,-10, ,42,37, ,-9,38, ,15,1,37, ,1,2,10,12,41, ,-3,-15,40, ,-7,-2,-6,4,8,-11,-1,1,10,47, " +
                 ",-1,46, ,5,4,5,-5,8,8,-20,-8,20,2,-6,4,-8,43,47,",
             encoderState.encode("Congratulations on completing the first set of Levels. More are soon to come!"))
-        TODO("Test decimal input")
-        assertEquals("19,49,33, ,19,2,31,50,", encoderState.encode("Brat Cat10"))
+        assertEquals("-1,-68,-48,49,33, ,19,2,31,50,", encoderState.encode("Brat Cat10"))
+        assertEquals("1,2,2,2,2,2,2,2,2,-15,", encoderState.encode("0123456789"))
     }
 }
